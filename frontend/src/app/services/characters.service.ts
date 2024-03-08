a68697924c34d0d45aa9c619aab645b764d4a21c
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-03-07 21:32:59 -0300

    testes api

--- frontend/src/app/services/characters.service.ts
+++ frontend/src/app/services/characters.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Characters } from '../models/Characters';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,11 @@ export class CharactersService {
   constructor(private httpClient: HttpClient) { }
   
   public getCharacterById(id:String):Observable<Characters>{
-    return this.httpClient.get<Characters>(`https://backend-marvel-api-9hny.vercel.app/marvel-api/${id}/${process.env['PASSWORD_API']}`)
+    return this.httpClient.get<Characters>(`https://backend-marvel-api-9hny.vercel.app/marvel-api/${id}/${environment.api}`)
   }
 
   public getCharacters():Observable<Characters[]>{
-    return this.httpClient.get<Characters[]>(`https://backend-marvel-api-9hny.vercel.app/marvel-api/${process.env['PASSWORD_API']}}`)
+    return this.httpClient.get<Characters[]>(`https://backend-marvel-api-9hny.vercel.app/marvel-api/${environment.api}}`)
   }
 
 }
