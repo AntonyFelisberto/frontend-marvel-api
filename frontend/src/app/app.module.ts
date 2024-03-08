2e1c79d1dbf70ff8eb74ff91d5d23a4aa8585a61
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-03-07 21:24:23 -0300

    ajustes de injeção de dependencia

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
@@ -18,7 +18,8 @@ import { FilterPipe } from './filters/filter-pipe.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
